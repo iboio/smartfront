29d87d04db0cbfe4fd07f6d637ecc51a605d1bac
lordmusamba <ibrahimkilic37@gmail.com>
2022-12-21 23:49:34 +0300

    smart house

---
+++ src/App/Components/Cards/CardBody/CardBox.js
@@ -0,0 +1,40 @@
+import {Box, Grid, Paper} from "@mui/material";
+import ThermostatIcon from "@mui/icons-material/Thermostat";
+import Typography from "@mui/material/Typography";
+import * as React from "react";
+
+
+export default function informationBox(title,data) {
+    return (
+        <Grid container>
+            <Grid item xs={6} padding={1}>
+                <Paper elevation={12}>
+                    <Box>
+                        <Grid container alignItems={"center"}>
+                            <Grid item xs={4}>
+                                <Grid container display={"flex"}>
+                                    <Grid item xs={4} p={1}>
+                                        <ThermostatIcon sx={{
+                                            fontSize: 70,
+                                        }}/>
+                                    </Grid>
+
+                                </Grid>
+                            </Grid>
+                            <Grid item xs={8}>
+                                <Grid container textAlign={"center"}>
+                                    <Grid item xs={12}>
+                                        <Typography padding={1} variant={"h5"}>{title}</Typography>
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <Typography variant={"h4"}>+{data}°C</Typography>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Paper>
+            </Grid>
+        </Grid>
+    )
+}
