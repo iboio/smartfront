b5d2a111bdb99e85344974f11be95669ff2ccffe
iboio <ibrahimkilic37@gmail.com>
2023-03-09 19:52:54 +0300

    first commit

--- src/App/Components/Box/Humidity.js
+++ src/App/Components/Box/Humidity.js
@@ -1,18 +1,19 @@
 import {Box, Grid, Paper} from "@mui/material";
-import ThermostatIcon from "@mui/icons-material/Thermostat";
+import OpacityIcon from '@mui/icons-material/Opacity';
 import Typography from "@mui/material/Typography";
 import * as React from "react";
 
-export default function Temperature({title, data}) {
+
+export default function Humidity(props) {
     return (
-        <Grid item xs={6} padding={1}>
+        <Grid item padding={1}>
             <Paper elevation={12}>
                 <Box>
                     <Grid container alignItems={"center"}>
                         <Grid item xs={4}>
                             <Grid container display={"flex"}>
                                 <Grid item xs={4} p={1}>
-                                    <ThermostatIcon sx={{
+                                    <OpacityIcon sx={{
                                         fontSize: 70,
                                     }}/>
                                 </Grid>
@@ -21,10 +22,10 @@ export default function Temperature({title, data}) {
                         <Grid item xs={8}>
                             <Grid container textAlign={"center"}>
                                 <Grid item xs={12}>
-                                    <Typography padding={1} variant={"h5"}>{title}</Typography>
+                                    <Typography padding={1} variant={"h5"}>{props.title}</Typography>
                                 </Grid>
                                 <Grid item xs={12}>
-                                    <Typography variant={"h4"}>+{data} °C</Typography>
+                                    <Typography variant={"h4"}>+{props.data} %</Typography>
                                 </Grid>
                             </Grid>
                         </Grid>
--- src/App/Components/Box/Temperature.js
+++ src/App/Components/Box/Temperature.js
@@ -3,10 +3,9 @@ import ThermostatIcon from "@mui/icons-material/Thermostat";
 import Typography from "@mui/material/Typography";
 import * as React from "react";
 
-
-export default function SoilMoisture({title, data}) {
+export default function Temperature(props) {
     return (
-        <Grid item xs={6} padding={1}>
+        <Grid item padding={1}>
             <Paper elevation={12}>
                 <Box>
                     <Grid container alignItems={"center"}>
@@ -22,10 +21,10 @@ export default function SoilMoisture({title, data}) {
                         <Grid item xs={8}>
                             <Grid container textAlign={"center"}>
                                 <Grid item xs={12}>
-                                    <Typography padding={1} variant={"h5"}>{title}</Typography>
+                                    <Typography padding={1} variant={"h5"}>{ props.title }</Typography>
                                 </Grid>
                                 <Grid item xs={12}>
-                                    <Typography variant={"h4"}>+{data}°C</Typography>
+                                    <Typography variant={"h4"}>+{ props.data } °C</Typography>
                                 </Grid>
                             </Grid>
                         </Grid>
